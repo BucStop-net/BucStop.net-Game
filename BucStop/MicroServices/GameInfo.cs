54031ec5a6612b965842cf0a0cedd9c76d1e8e71
William Roe <103656762+ProdigyWill@users.noreply.github.com>
2024-04-16 11:25:23 -0400

    Revert "Updated Docker Ignore so that game scores/play counts aren't changed in rollback."
    This reverts commit 3e469d91d3e7a0b9aebea174f010a23ad0c3e8e0.

--- BucStop/MicroServices/GameInfo.cs
+++ BucStop/MicroServices/GameInfo.cs
@@ -7,6 +7,5 @@
         public string Description { get; set; }
         public string DateAdded { get; set; }
         public string HowTo { get; set; }
-        public string Content { get; set; }
     }
 }
